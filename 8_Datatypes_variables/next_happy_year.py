032a7f3e349b6b56bb9545722cf8fa36eb94da27
h-dmt <113796359+h-dmt@users.noreply.github.com>
2022-09-21 23:28:03 +0300

    finished

--- 8_Datatypes_variables/next_happy_year.py
+++ 8_Datatypes_variables/next_happy_year.py
@@ -3,6 +3,12 @@ You are saying goodbye to your best friend: "See you next happy year".
 Happy Year is the year with only distinct digits, for example, 2018.
 Write a program that receives an integer number and finds the next happy year.
 """
-year = str(input())
-
-while
+year = int(input())
+year_set = set()
+happy_year = year
+while len(year_set) != len(str(happy_year)):
+    year_set = set()
+    happy_year += 1
+    for i in str(happy_year):
+        year_set.add(i)
+print(f'{happy_year}')
