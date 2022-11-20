82a852580404627f62d6fc684e95ead7125989f3
h-dmt <113796359+h-dmt@users.noreply.github.com>
2022-11-20 13:01:23 +0200

    regex new version

--- Dictionaries/exam_results.py
+++ Dictionaries/exam_results.py
@@ -15,3 +15,46 @@ format:
 …
 {usernameN} | {points}"
 """
+
+
+def fill_data(dict_submissions, username, language, points):
+
+    if username not in dict_submissions:  # if new user add user
+        dict_submissions[username] = {language: points}
+        # if not first submission
+    elif username in dict_submissions and language in dict_submissions[username]:
+        if points > dict_submissions[username][language]:  # save only the highest score
+            dict_submissions[username][language] = points
+        #  if first submission for the language
+    elif username in dict_submissions and language not in dict_submissions[username]:
+        dict_submissions[username][language] = points
+
+    return dict_submissions
+
+
+submission = input().split('-')
+dictionary_submissions = {}
+languages_submission = {}
+
+while submission[0] != 'exam finished':
+    if len(submission) == 3:
+        user, lang, p = submission[:]
+        if lang not in languages_submission:
+            languages_submission[lang] = 1
+        else:
+            languages_submission[lang] += 1
+        dictionary_submissions = fill_data(dictionary_submissions, user, lang, p)
+    elif len(submission) == 2:
+        user = submission[0]
+        dictionary_submissions.pop(user)
+    submission = input().split('-')
+#  Print output
+print('Results:')
+for key in dictionary_submissions:  # for every student in dictionary
+    for lecture in dictionary_submissions[key]:  # for every language per student
+        score = dictionary_submissions[key][lecture]  # get the score
+        print(f"{key} | {score}")
+print('Submissions:')
+for title in languages_submission:
+    n = languages_submission[title]
+    print(f"{title} - {n}")
--- Regular_expressions/match_numbers.py
+++ Regular_expressions/match_numbers.py
@@ -2,8 +2,14 @@ import re
 
 constrains = r"((^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s)))"
 
-num_input = input().split()
-for num in num_input:
-    matches = re.search(constrains, num)
-    if matches:
-        print(matches.group(), end=' ')
+num_input = input()
+matches = re.findall(constrains, num_input)
+for number in matches:
+    print(number[0], end=' ')
+
+# with .search()
+# num_input = input().split()
+# for num in num_input:
+#    matches = re.search(constrains, num)
+#    if matches:
+#       print(matches.group(), end=' ')
